fd106ec45667f32aeeab86e4edfe6c7bcd32c316
Sandeep Kumar <sandeep.kumar@kochartech.com>
2021-10-22 15:58:54 +0530

    Java Data Class (POJP) added

---
+++ User.java
@@ -0,0 +1,28 @@
+
+/**
+ * Created on 22-10-2021.
+ *
+ * @author Sandeep Kumar (https://github.com/sandeep9094)
+ */
+
+public class User {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
